a8493876a4ca5a181646915208aca33ca0950265
Harwin Cuestas <Harwin@192.168.1.5>
2024-04-20 11:27:02 -0500

    add name title end

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Harwin Calculator</div>
+        <div>CIDR Calculator Harwin</div>
       </header>
       <IPv4Addr />
       <footer>
